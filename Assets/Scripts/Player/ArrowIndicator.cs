cbc1f2cf743e8cf257699bf8e683517f9d40629b
liozmadar <liozmadar@gmail.com>
2022-10-22 20:32:32 +0300

    Fix the arrowIndicator distance meter

--- Assets/Scripts/Player/ArrowIndicator.cs
+++ Assets/Scripts/Player/ArrowIndicator.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ArrowIndicator : MonoBehaviour
 {
     public Image image;
     public Transform target;
+    public TextMeshProUGUI meter;
+    public Vector3 offSet;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +27,12 @@ public class ArrowIndicator : MonoBehaviour
         float maxY = Screen.height - minY;
 
 
-        Vector2 pos = Camera.main.WorldToScreenPoint(target.position);
+        Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offSet);
 
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
         image.transform.position = pos;
+        meter.text = ((int)Vector3.Distance(target.position, transform.position)).ToString() + "m";
     }
 }
