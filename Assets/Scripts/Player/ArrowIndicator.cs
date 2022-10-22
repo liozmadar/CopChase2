a36ae62f04e1172ef251fc19a35e093535fa4003
liozmadar <liozmadar@gmail.com>
2022-10-22 20:19:50 +0300

    Added an arrowIndicator on screen

---
+++ Assets/Scripts/Player/ArrowIndicator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowIndicator : MonoBehaviour
+{
+    public Image image;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float minX = image.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+
+        float minY = image.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.height - minY;
+
+
+        Vector2 pos = Camera.main.WorldToScreenPoint(target.position);
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        image.transform.position = pos;
+    }
+}
