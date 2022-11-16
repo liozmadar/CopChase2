9cf9ac04b32d904d6a8e69d61ef26fc066f698f2
liozmadar <liozmadar@gmail.com>
2022-11-16 15:52:29 +0200

    Added a saving best scores

--- Assets/Scripts/Canvas/CanvasManager.cs
+++ Assets/Scripts/Canvas/CanvasManager.cs
@@ -11,9 +11,17 @@ public class CanvasManager : MonoBehaviour
 
     public GameObject endGameCard;
     public TextMeshProUGUI headerText;
+
     public TextMeshProUGUI timerScore;
     public TextMeshProUGUI copsDestroyed;
     public TextMeshProUGUI coneCollected;
+
+    public TextMeshProUGUI bestScoreText;
+    public int bestScoreCount;
+    public TextMeshProUGUI mostCopsDestroyedText;
+    public int mostCopsDestroyedCount;
+    public TextMeshProUGUI mostConeCollectedText;
+    public int mostConeCollectedCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +31,7 @@ public class CanvasManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        CheckTheBestScoreNumber();
     }
     public void TryAgainButton()
     {
@@ -44,5 +52,47 @@ public class CanvasManager : MonoBehaviour
         timerScore.text = Timer.instance.timerText.ToString();
         copsDestroyed.text = GameManager.instance.copsDestroyedNumber.ToString();
         coneCollected.text = Cones.instance.coneCollectedCount.ToString();
+    }
+    void MostConesCollectedCheck()
+    {
+        mostConeCollectedCount = Cones.instance.coneCollectedCount;
+        PlayerPrefs.SetInt("Cones", mostConeCollectedCount);
+    }
+    void MostCopsDestroyed()
+    {
+        mostCopsDestroyedCount = GameManager.instance.copsDestroyedNumber;
+        PlayerPrefs.SetInt("Cops", mostCopsDestroyedCount);
+    }
+    void BestScore()
+    {
+        bestScoreCount = Timer.instance.timerText;
+        PlayerPrefs.SetInt("Score", bestScoreCount);
+    }
+    void CheckTheBestScoreNumber()
+    {
+        if (Cones.instance.coneCollectedCount > PlayerPrefs.GetInt("Cones"))
+        {
+            MostConesCollectedCheck();
+        }
+        else
+        {
+            mostConeCollectedText.text = PlayerPrefs.GetInt("Cones").ToString();
+        }
+        if (GameManager.instance.copsDestroyedNumber > PlayerPrefs.GetInt("Cops"))
+        {
+            MostCopsDestroyed();
+        }
+        else
+        {
+            mostCopsDestroyedText.text = PlayerPrefs.GetInt("Cops").ToString();
+        }
+        if (Timer.instance.timerText > PlayerPrefs.GetInt("Score"))
+        {
+            BestScore();
+        }
+        else
+        {
+            bestScoreText.text = PlayerPrefs.GetInt("Score").ToString();
+        }
     }
 }
