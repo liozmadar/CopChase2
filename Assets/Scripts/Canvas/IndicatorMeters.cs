e4167930319b5905f6b96cbdb080675e683d27e0
liozmadar <liozmadar@gmail.com>
2022-12-23 12:52:18 +0200

    Cone meters position now "good" offSet position is not good enough

--- Assets/Scripts/Canvas/IndicatorMeters.cs
+++ Assets/Scripts/Canvas/IndicatorMeters.cs
@@ -12,7 +12,7 @@ public class IndicatorMeters : MonoBehaviour
     private TextMeshProUGUI metersText;
     private GameObject canvas;
 
-    public Vector3 offSet;
+    private Vector3 offSet = new Vector3(0,50,0);
 
     // Start is called before the first frame update
     void Start()
