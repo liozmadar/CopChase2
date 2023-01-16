09c093782b22125795f70dffb4c5d8719cfaee85
liozmadar <liozmadar@gmail.com>
2023-01-16 20:49:29 +0200

    Trying to fix the ground follow player

--- Assets/Scripts/Player/GroundFollow.cs
+++ Assets/Scripts/Player/GroundFollow.cs
@@ -1,10 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundFollow : MonoBehaviour
 {
     public GameObject player;
+    public GameObject groundFloor;
+    float spawnFloorTimer = 5;
+    public GameObject newGroundFloor;
+    public List<GameObject> newGroundFloors;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +21,35 @@ public class GroundFollow : MonoBehaviour
         {
             player = GameObject.FindGameObjectWithTag("Player");
 
+            spawnFloorTimer -= Time.deltaTime;
+            if (spawnFloorTimer <= 0)
+            {
+                SpawnGroundFloor();
+                spawnFloorTimer = 5;
+                if (newGroundFloors.Count >= 2)
+                {
+                    Destroy(newGroundFloors[0]);
+                    newGroundFloors.RemoveAt(0);
+                }
+            }
+        }
+    }
+    void GroundFloorFollowPlayer()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") == isActiveAndEnabled)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+
             var posX = player.transform.position.x;
             var posZ = player.transform.position.z;
             var newPos = new Vector3(posX, 0, posZ);
             gameObject.transform.position = newPos;
         }
+    }
+    void SpawnGroundFloor()
+    {
+        Vector3 newPos = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+        newGroundFloor = Instantiate(groundFloor, newPos, Quaternion.identity);
+        newGroundFloors.Add(newGroundFloor);
     }
 }
